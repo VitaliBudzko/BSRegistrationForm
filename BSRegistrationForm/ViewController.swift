e953a6ca27971cba96e7db53e515411ff8c70448
Vitali Budzko <labanka1983@gmail.com>
2019-09-20 21:13:46 +0300

    Initial Commit

---
+++ BSRegistrationForm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BSRegistrationForm
+//
+//  Created by Sasha Dubikovskaya on 9/20/19.
+//  Copyright © 2019 BudzkoVitali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
